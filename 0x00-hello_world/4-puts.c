4ac30d87199e8badedaec44d3b3fdbe2eef54000
Xhamm2010 <46625640+Xhamm2010@users.noreply.github.com>
2022-03-12 08:59:53 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main-entry point
-* return type is 0 for success
+*main-entry point
+*return type is 0 for success
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
